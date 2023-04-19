e205ccdb967f1747630f62e7267c453d325dc50d
peter <pkmuthaka@gmail.com>
2023-04-19 14:30:26 +0300

    add admin login command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,13 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+// --Command to login admin user to the petshop UI
+Cypress.Commands.add('loginAdmin', (email, password) => {
+    // clear all cookies
+    cy.clearCookies()
+    // find email input element and type the admin email
+    cy.get('#input-0').clear().type(email)
+    // find password input element and type the admin password
+    cy.get('#input-2').clear().type(password)
+    // find the submit button and click
+    cy.get('.v-btn').click()
+    
+})
